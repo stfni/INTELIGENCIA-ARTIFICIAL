db6145dd6c6429585247f40cf47c7e52813df206
Stefani Vitória Tavares de Carvalho <110827044+stfni@users.noreply.github.com>
2024-06-24 10:56:23 -0300

    terceiro ano

--- script.js
+++ script.js
@@ -100,3 +100,20 @@ function mostraAlternativas() {
 }
 
 mostraPergunta();
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historia += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+}
+
+enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que… alternativas: [
+
+        texto: "Isso é assustador!",
+        afirmacao: "No início ficou com medo do que essa tecnologia pode fazer.
+    {
+        texto: "Isso é maravilhoso!",
+        afirmacao: "Quis saber como usar IA no seu dia a dia."
+    }
+    
